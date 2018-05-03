b03490f734634d39cf55c16a04d1ca06233a7611
Samra Salihic <ssalihic1@etf.unsa.ba>
2018-05-03 23:13:13 +0200

    Radinoica 2

---
+++ BIMManager.API/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BIMManager.Data;
+
+namespace BIMManager.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Project")]
+    public class ProjectController : Controller
+    {
+        BIMManagerContext context;
+        public ProjectController()
+        {
+
+        }
+        public ICollection<Models.Entities.Project> getAll()
+        {
+            return context.Projects.ToList();
+         
+        }
+
+    }
+}
